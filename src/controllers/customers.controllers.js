1c059eeb3f858839beb31b1ca632286f33653996
Tulio Barcena <produccion@agencianuba.com>
2023-02-09 20:21:05 -0600

    crear usuario

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -1,5 +1,9 @@
 const Customer = require('./../models/customersSchema');
 const { respApi } = require('../helpers/helpers');
+const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
+require('dotenv').config();
+
 
 const getCustomers = async(req,res) =>{
     const Customers = await Customer.find({})
@@ -7,8 +11,52 @@ const getCustomers = async(req,res) =>{
 }
 
 const createCustomers = async(req,res) =>{
-    const CustomerNew = await Customer.create(req.body)
-    respApi(res, 'exito', CustomerNew);
+    const{ name, email, password } = req.body
+
+    try{
+        //generar aleatorio
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
+        //crear usuario con pass encriptado
+        const respuestaBD = await Customer.create({
+            name,
+            email,
+            password: hashedPassword,
+        })
+
+         //crear el jsonwebtoken
+    const payload = {
+        user: {
+            id: respuestaBD._id,
+        },
+    }
+
+    //firmar el jwtoken
+
+    jwt.sign(
+        payload,
+        process.env.SECRET,
+        {
+            expiresIn: 360000,
+        },
+        (error, token) =>{
+            if(error) throw error
+            res.json(respuestaBD)
+        }
+
+        
+    )
+
+
+        respApi(res, 'exito', CustomerNew);
+    }catch(error){
+       return res.status(400).json({
+        msg: error,
+       }
+       )
+    }
+
     }
 
 const updateCustomers = async(req,res) =>{
